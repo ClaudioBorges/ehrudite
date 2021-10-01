6d1fb48c375f8b572c09befe6b683d269d6da0be
Claudio Borges <clborges@amazon.com>
2021-09-30 22:40:36 -0300

    Add BOS and EOS on training pipeline

--- src/ehrudite/core/pipeline.py
+++ src/ehrudite/core/pipeline.py
@@ -22,55 +22,16 @@ sentencepiece_base_path = os.path.join(tokenizer_base_path, "sentencepiece/")
 wordpiece_base_path = os.path.join(tokenizer_base_path, "wordpiece/")
 checkpoint_path = os.path.join(ehr_data_path, "checkpoint/train")
 
+PAD_TOK = 0
+BOS_TOK = 1
+EOS_TOK = 2
+UNK_TOK = 3
+
 
 class EhruditePipeline:
     def __init__(self, ehrpreper_file, tokenizer):
         self._ehrpreper_file = ehrpreper_file
         self._tokenizer = tokenizer
-
-    def _make_vocabulary(self, tokenizer, output_path, **kwargs):
-        tokenizer.generate_vocab_file_from_texts(texts, output_path, **kwargs)
-
-    def _split_train_test(self, documents):
-        pass
-
-    def _make_generator(self):
-        sentences = self._make_contents()
-        sequences = (
-            self._tokenizer.tokenize(sentence).numpy().tolist()
-            for sentence in sentences
-        )
-        for i, sequence in enumerate(sequences):
-            yield sequence
-            if i == 100:
-                break
-
-    def _tf_run(self):
-        repeatable_gen = er_text.RepeatableGenerator(self._make_generator)
-
-        # model = GloveModel(embedding_size=300, context_size=10)
-        # model.fit_to_corpus(repeatable_gen)
-        # model.train(num_epochs=100)
-
-        skipgram = SkipgramModel()
-        skipgram.fit_to_corpus(repeatable_gen)
-        skipgram.train(epochs=20, window=100, workers=16)
-        lll = list(model.wv.key_to_index.keys())
-
-        # model.save("word2vec.model")
-
-        # embedding = tf_skipgram.SkipGramModel(
-        #    vocab_size=tokenizer.vocab_size().numpy(),
-        #    embedding_dim=128,
-        #    n_negative_samples=3,
-        #    window_size=4,
-        # )
-
-        # sentence_ds = self._make_content_sentence_ds(self._ehrpreper_file)
-        # sentence_ds = sentence_ds.cache().batch(1024).prefetch(tf.data.AUTOTUNE)
-        # sequence_ds = sentence_ds.map(self._tokenizer.tokenize).unbatch()
-        # embedding.run(sequence_ds.take(1000))
-        # embedding.save("./skipgram.model")
 
     def run(self):
         self._tf_run()
@@ -79,28 +40,26 @@ class EhruditePipeline:
         # Word2Vec
         pass
 
-    def _document_annotations_join(self, annotations, separator=" "):
+    def _join_annotations(self, annotations, separator=" "):
         return separator.join(annotations)
 
-    def _document_to_xy(self, documents):
+    def _prepare_and_split_x_y(self, documents):
         return (
             (
                 er_text.preprocess_text(document.content),
-                er_text.preprocess_text(
-                    self._document_annotations_join(document.annotations)
-                ),
+                er_text.preprocess_text(self._join_annotations(document.annotations)),
             )
             for i, document in enumerate(documents)
         )
 
-    def _data_xy_gen(self):
-        func = lambda: self._document_to_xy(
+    def _ehrpreper_gen(self):
+        func = lambda: self._prepare_and_split_x_y(
             ehrpreper.document_entity_generator(self._ehrpreper_file)
         )
         return er_text.LenghtableRepeatableGenerator(func)
 
-    def _data_xy_k_fold_gen(self, n_splits=4):
-        data = self._data_xy_gen()
+    def _ehrpreper_k_fold_gen(self, n_splits=4):
+        data = self._ehrpreper_gen()
         logging.info(f"_k_fold_gen (n_splits={n_splits}, data_len={len(data)})")
         kf = skl_msel.KFold(n_splits=n_splits)
         # Make indexes to be sure data can be a generator
@@ -129,91 +88,9 @@ class EhruditePipeline:
                 test,
             )
 
-    def _make_document_k_fold_xy_ds(self):
-        generator = self._data_xy_k_fold_gen()
-        return tf.data.Dataset.from_generator(
-            generator,
-            output_signature=(
-                tf.TensorSpec(shape=(), dtype=tf.string),
-                tf.TensorSpec(shape=(), dtype=tf.string),
-            ),
-        )
 
-    def _document_to_tuple(self, documents):
-        return (
-            (
-                document.content,
-                document.annotations,
-            )
-            for document in documents
-        )
-
-    def _make_document_ds(self):
-        func = lambda: self._document_to_tuple(
-            ehrpreper.document_entity_generator(self._ehrpreper_file)
-        )
-        generator = er_text.RepeatableGenerator(func)
-        return tf.data.Dataset.from_generator(
-            generator,
-            output_signature=(
-                tf.TensorSpec(shape=(), dtype=tf.string),
-                tf.TensorSpec(shape=(None,), dtype=tf.string),
-            ),
-        )
-
-    def _make_contents(self):
-        texts = ehrpreper.data_generator(self._ehrpreper_file)
-        preprocessed = er_text.preprocess(texts)
-        return preprocessed
-
-    def _make_content_sentence_ds(self, ehrpreper_file):
-        sentences = self._make_contents()
-        return tf.data.Dataset.from_generator(
-            er_text.Generator(sentences), output_types=tf.string, output_shapes=()
-        )
-        # def _make_ehr_dataset(ehrpreper_file):
-        #    def model_iterator():
-        #        return (
-        #            (document.content, document.annotations)
-        #            for model in ehrpreper.load(ehrpreper_file)
-        #            for document in model.documents
-        #        )
-
-        #    return tf.data.Dataset.from_generator(
-        #        er_text.Generator(model_iterator()),
-        #        output_signature=(
-        #            tf.TensorSpec(shape=(), dtype=tf.string),
-        #            tf.TensorSpec(shape=None, dtype=tf.string),
-        #        ),
-        #    )
-
-        # ehr_ds = self._make_ehr_dataset(ehrpreper_file)
-
-        ## [TODO] split the same as python string splitlines
-        # sentence_ds = (
-        #    ehr_ds.map(lambda content, annotations: content)
-        #    .map(lambda content: tf.strings.lower(content))
-        #    .flat_map(lambda x: tf.data.Dataset.from_tensor_slices(tf.strings.split(x, '\n')))
-        #    .flat_map(lambda x: tf.data.Dataset.from_tensor_slices(tf.strings.split(x, '\r')))
-        #    .flat_map(lambda x: tf.data.Dataset.from_tensor_slices(tf.strings.split(x, '\v')))
-        #    .flat_map(lambda x: tf.data.Dataset.from_tensor_slices(tf.strings.split(x, '\f')))
-        #    .flat_map(lambda x: tf.data.Dataset.from_tensor_slices(tf.strings.split(x, '\x1c')))
-        #    .flat_map(lambda x: tf.data.Dataset.from_tensor_slices(tf.strings.split(x, '\x1d')))
-        #    .flat_map(lambda x: tf.data.Dataset.from_tensor_slices(tf.strings.split(x, '\x1e')))
-        #    .flat_map(lambda x: tf.data.Dataset.from_tensor_slices(tf.strings.split(x, '\x85')))
-        #    .flat_map(lambda x: tf.data.Dataset.from_tensor_slices(tf.strings.split(x, '\u2028')))
-        #    .flat_map(lambda x: tf.data.Dataset.from_tensor_slices(tf.strings.split(x, '\u2029')))
-        #    .filter(lambda sentence: tf.strings.length(sentence) != 0)
-        # )
-        # return sentence_ds
-
-    def _load_contents_from_ehrpreper(ehrpreper_file_name):
-        logging.info(f"_load_contents_from_ehrpreper (file_name={ehrpreper_file_name})")
-        return (
-            document.content
-            for model in ehrpreper.load(ehrpreper_file_name)
-            for document in model.documents
-        )
+def _enclose_tensor_with_bos_eos(tensor):
+    tf.concat([[BOS_TOK], tensor, [EOS_TOK]], 0)
 
 
 def _unpack_data_xy(data):
@@ -277,24 +154,33 @@ def _train_model(run_id, train_xy, test_xy):
         os.path.join(wordpiece_base_path, train_y_id)
     )
 
-    MAX_LENGTH = 512
-    PAD_VAL = 0
+    tok_s = sentencepiece.SentencepieceTokenizer(
+        os.path.join(sentencepiece_base_path, train_x_id)
+    )
+
+    X_MAX_LEN = 1024
+    Y_MAX_LEN = 128
 
     def _prepare_gen():
-        def _normalize(sequences):
-            return tf.keras.preprocessing.sequence.pad_sequences(
-                sequences,
-                maxlen=MAX_LENGTH,
+        def normalize(sequence, maxlen):
+            sliced = tf.slice(sequence, [0], [min(maxlen - 2, sequence.shape[0])])
+            enclosed = tf.concat([[BOS_TOK], sliced, [EOS_TOK]], 0)
+            padded = tf.keras.preprocessing.sequence.pad_sequences(
+                [enclosed],
+                maxlen=maxlen,
                 padding="post",
                 truncating="post",
-                value=PAD_VAL,
-            )
+                value=PAD_TOK,
+            )[0]
+            return tf.constant(padded)
 
         train_x_tok = (tok_input.tokenize(x) for x in train_x)
         train_y_tok = (tok_target.tokenize(y) for y in train_y)
 
-        norm = (_normalize((x, y)) for x, y in zip(train_x_tok, train_y_tok))
-        return ((tf.constant(seqs[0]), tf.constant(seqs[1])) for seqs in norm)
+        return (
+            (normalize(x, X_MAX_LEN), normalize(y, Y_MAX_LEN))
+            for x, y in zip(train_x_tok, train_y_tok)
+        )
 
     train_xy_tok_gen = er_text.RepeatableGenerator(_prepare_gen)
 
@@ -315,7 +201,6 @@ def _train_model(run_id, train_xy, test_xy):
         )
 
     train_batches = make_batches(train_xy_tok_ds)
-    pdb.set_trace()
 
     # From https://www.tensorflow.org/text/tutorials/transformer
     num_layers = 4
@@ -364,9 +249,7 @@ def _train_model(run_id, train_xy, test_xy):
 
     optimizer = transformer_m.optimizer(d_model)
 
-    ckpt = tf.train.Checkpoint(
-        transformer=transformer, optimizer=optimizer
-    )
+    ckpt = tf.train.Checkpoint(transformer=transformer, optimizer=optimizer)
 
     ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
 
@@ -440,7 +323,7 @@ if __name__ == "__main__":
     for run_id, (
         train_xy,
         test_xy,
-    ) in enumerate(ehr._data_xy_k_fold_gen()):
+    ) in enumerate(ehr._ehrpreper_k_fold_gen()):
         # _train_tokenizer(run_id, train_xy, test_xy)
         _train_model(run_id, train_xy, test_xy)
 
