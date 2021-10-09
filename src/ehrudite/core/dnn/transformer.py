6fd820c25d11225b7ec3c648e99aac4b1f181e9e
Claudio Borges <clborges@amazon.com>
2021-10-09 10:36:17 -0300

    Does not use keras MultiHeadAttention

--- src/ehrudite/core/dnn/transformer.py
+++ src/ehrudite/core/dnn/transformer.py
@@ -193,9 +193,7 @@ class EncoderLayer(tf.keras.layers.Layer):
         """
 
         super(EncoderLayer, self).__init__()
-        self.mha = tf.keras.layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=d_model, value_dim=d_model
-        )
+        self.mha = MultiHeadAttention(num_heads=num_heads, d_model=d_model)
         self.ffn = _point_wise_feed_forward_network(d_model, dff)
 
         self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
@@ -205,7 +203,7 @@ class EncoderLayer(tf.keras.layers.Layer):
         self.dropout2 = tf.keras.layers.Dropout(rate)
 
     def call(self, x, training, mask):
-        attn_output = self.mha(x, x, x, mask)  # (batch_size, input_seq_len, d_model)
+        attn_output, _ = self.mha(x, x, x, mask)  # (batch_size, input_seq_len, d_model)
         attn_output = self.dropout1(attn_output, training=training)
         out1 = self.layernorm1(x + attn_output)  # (batch_size, input_seq_len, d_model)
 
@@ -229,12 +227,8 @@ class DecoderLayer(tf.keras.layers.Layer):
     def __init__(self, d_model, num_heads, dff, rate=0.1):
         super(DecoderLayer, self).__init__()
 
-        self.mha1 = tf.keras.layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=d_model, value_dim=d_model
-        )
-        self.mha2 = tf.keras.layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=d_model, value_dim=d_model
-        )
+        self.mha1 = MultiHeadAttention(num_heads=num_heads, d_model=d_model)
+        self.mha2 = MultiHeadAttention(num_heads=num_heads, d_model=d_model)
 
         self.ffn = _point_wise_feed_forward_network(d_model, dff)
 
@@ -250,17 +244,13 @@ class DecoderLayer(tf.keras.layers.Layer):
         # enc_output.shape == (batch_size, input_seq_len, d_model)
 
         attn1, attn_weights_block1 = self.mha1(
-            x, x, x, attention_mask=look_ahead_mask, return_attention_scores=True
+            x, x, x, look_ahead_mask
         )  # (batch_size, target_seq_len, d_model)
         attn1 = self.dropout1(attn1, training=training)
         out1 = self.layernorm1(attn1 + x)
 
         attn2, attn_weights_block2 = self.mha2(
-            out1,
-            enc_output,
-            enc_output,
-            attention_mask=padding_mask,
-            return_attention_scores=True,
+            enc_output, enc_output, out1, padding_mask
         )  # (batch_size, target_seq_len, d_model)
         attn2 = self.dropout2(attn2, training=training)
         out2 = self.layernorm2(attn2 + out1)  # (batch_size, target_seq_len, d_model)
@@ -338,6 +328,98 @@ def _positional_encoding(position, d_model):
 
     pos_encoding = angle_rads[np.newaxis, ...]
     return tf.cast(pos_encoding, dtype=tf.float32)
+
+
+def scaled_dot_product_attention(q, k, v, mask):
+    """Calculate the attention weights.
+    q, k, v must have matching leading dimensions.
+    k, v must have matching penultimate dimension, i.e.: seq_len_k = seq_len_v.
+    The mask has different shapes depending on its type(padding or look ahead)
+    but it must be broadcastable for addition.
+
+    Args:
+      q: query shape == (..., seq_len_q, depth)
+      k: key shape == (..., seq_len_k, depth)
+      v: value shape == (..., seq_len_v, depth_v)
+      mask: Float tensor with shape broadcastable
+            to (..., seq_len_q, seq_len_k). Defaults to None.
+
+    Returns:
+      output, attention_weights
+    """
+    # (..., seq_len_q, seq_len_k)
+    matmul_qk = tf.matmul(q, k, transpose_b=True)
+
+    # scale matmul_qk
+    dk = tf.cast(tf.shape(k)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+
+    # add the mask to the scaled tensor.
+    if mask is not None:
+        scaled_attention_logits += mask * -1e9
+
+    # softmax is normalized on the last axis (seq_len_k) so that the scores
+    # add up to 1.
+    attention_weights = tf.nn.softmax(
+        scaled_attention_logits, axis=-1
+    )  # (..., seq_len_q, seq_len_k)
+
+    output = tf.matmul(attention_weights, v)  # (..., seq_len_q, depth_v)
+
+    return output, attention_weights
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        self.num_heads = num_heads
+        self.d_model = d_model
+
+        assert d_model % self.num_heads == 0
+
+        self.depth = d_model // self.num_heads
+
+        self.wq = tf.keras.layers.Dense(d_model)
+        self.wk = tf.keras.layers.Dense(d_model)
+        self.wv = tf.keras.layers.Dense(d_model)
+
+        self.dense = tf.keras.layers.Dense(d_model)
+
+    def split_heads(self, x, batch_size):
+        """Split the last dimension into (num_heads, depth).
+        Transpose the result such that the shape is (batch_size, num_heads, seq_len, depth)
+        """
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, v, k, q, mask):
+        batch_size = tf.shape(q)[0]
+
+        q = self.wq(q)  # (batch_size, seq_len, d_model)
+        k = self.wk(k)  # (batch_size, seq_len, d_model)
+        v = self.wv(v)  # (batch_size, seq_len, d_model)
+
+        q = self.split_heads(q, batch_size)  # (batch_size, num_heads, seq_len_q, depth)
+        k = self.split_heads(k, batch_size)  # (batch_size, num_heads, seq_len_k, depth)
+        v = self.split_heads(v, batch_size)  # (batch_size, num_heads, seq_len_v, depth)
+
+        # scaled_attention.shape == (batch_size, num_heads, seq_len_q, depth)
+        # attention_weights.shape == (batch_size, num_heads, seq_len_q, seq_len_k)
+        scaled_attention, attention_weights = scaled_dot_product_attention(
+            q, k, v, mask
+        )
+
+        scaled_attention = tf.transpose(
+            scaled_attention, perm=[0, 2, 1, 3]
+        )  # (batch_size, seq_len_q, num_heads, depth)
+
+        concat_attention = tf.reshape(
+            scaled_attention, (batch_size, -1, self.d_model)
+        )  # (batch_size, seq_len_q, d_model)
+
+        output = self.dense(concat_attention)  # (batch_size, seq_len_q, d_model)
+
+        return output, attention_weights
 
 
 def optimizer(d_model):
--- test/core/dnn/transformer_test.py
+++ test/core/dnn/transformer_test.py
@@ -7,11 +7,11 @@ import tensorflow as tf
 
 def test_transformer_encoder_decoder_layer():
     batch_size = random.randint(8, 64)
-    d_model = random.randint(128, 512)
+    d_model = 2 ** random.randint(7, 9)
     dff = random.randint(512, 2048)
     input_seq_len = random.randint(40, 50)
     target_seq_len = random.randint(10, 30)
-    num_heads = random.randint(2, 8)
+    num_heads = 2 ** random.randint(1, 4)
 
     encoder_layer = transformer.EncoderLayer(d_model, num_heads, dff)
     encoder_layer_output = encoder_layer(
@@ -32,12 +32,12 @@ def test_transformer_encoder_decoder_layer():
 
 def test_transformer_encoder_decoder():
     batch_size = random.randint(8, 64)
-    d_model = random.randint(128, 512)
+    d_model = 2 ** random.randint(7, 9)
     dff = random.randint(512, 2048)
     input_seq_len = random.randint(40, 50)
     input_vocab_size = random.randint(1000, 10000)
     maximum_position_encoding = random.randint(1024, 4096)
-    num_heads = random.randint(2, 8)
+    num_heads = 2 ** random.randint(1, 4)
     num_layers = random.randint(2, 4)
     target_seq_len = random.randint(10, 30)
     target_vocab_size = random.randint(1000, 10000)
@@ -82,18 +82,65 @@ def test_transformer_encoder_decoder():
 
 def test_transformer_positional_encoding():
     maximum_position_encoding = random.randint(1024, 4096)
-    d_model = random.randint(128, 512)
+    d_model = 2 ** random.randint(7, 9)
     pos_encoding = transformer._positional_encoding(maximum_position_encoding, d_model)
     assert pos_encoding.shape == (1, maximum_position_encoding, d_model)
 
 
+def test_transformer_scaled_dot_product_attention():
+    # Both K and V penultimate dimension must match
+    # Both K and Q leading dimension must mathc
+    temp_k = tf.constant(
+        [[10, 0, 0], [0, 10, 0], [0, 0, 10], [0, 0, 10]], dtype=tf.float32
+    )  # (4, 3)
+
+    temp_v = tf.constant(
+        [[1, 0], [10, 0], [100, 5], [1000, 6]], dtype=tf.float32
+    )  # (4, 2)
+
+    # This `query` aligns with the second `key`,
+    # so the second `value` is returned.
+    temp_q = tf.constant([[0, 10, 0]], dtype=tf.float32)  # (1, 3)
+
+    temp_out, temp_attn = transformer.scaled_dot_product_attention(
+        temp_q, temp_k, temp_v, None
+    )
+    assert temp_attn.shape == (temp_q.shape[0], temp_v.shape[0])
+    assert temp_out.shape == (temp_q.shape[0], temp_v.shape[1])
+
+    temp_q = tf.constant(
+        [[0, 0, 10], [0, 10, 0], [10, 10, 0]], dtype=tf.float32
+    )  # (3, 3)
+
+    temp_out, temp_attn = transformer.scaled_dot_product_attention(
+        temp_q, temp_k, temp_v, None
+    )
+    assert temp_attn.shape == (temp_q.shape[0], temp_v.shape[0])
+    assert temp_out.shape == (temp_q.shape[0], temp_v.shape[1])
+
+
+def test_multi_head_attention():
+    batch_size = random.randint(8, 64)
+    d_model = 2 ** random.randint(7, 9)
+    encoder_sequence = random.randint(50, 100)
+    num_heads = 2 ** random.randint(1, 4)
+
+    temp_mha = transformer.MultiHeadAttention(d_model=d_model, num_heads=num_heads)
+    y = tf.random.uniform(
+        (batch_size, encoder_sequence, d_model)
+    )  # (batch_size, encoder_sequence, d_model)
+    out, attn = temp_mha(y, k=y, q=y, mask=None)
+    assert out.shape == y.shape
+    assert attn.shape == (y.shape[0], num_heads, y.shape[1], y.shape[1])
+
+
 def test_transformer_model():
     batch_size = random.randint(8, 64)
-    d_model = random.randint(128, 512)
+    d_model = 2 ** random.randint(7, 9)
     dff = random.randint(512, 2048)
     input_seq_len = random.randint(40, 50)
     input_vocab_size = random.randint(1000, 10000)
-    num_heads = random.randint(2, 8)
+    num_heads = 2 ** random.randint(1, 4)
     num_layers = random.randint(2, 4)
     target_seq_len = random.randint(10, 30)
     target_vocab_size = random.randint(1000, 10000)
@@ -121,6 +168,6 @@ def test_transformer_model():
 
 
 def test_optimizer():
-    d_model = random.randint(128, 512)
+    d_model = 2 ** random.randint(7, 9)
     optimizer = transformer.optimizer(d_model)
     assert optimizer is not None
